512a2690f2a5a3118b41876a1a8dc5d2b13910d0
Amit Kumar <amitaquarius@gmail.com>
2026-09-02 00:04:37 +0530

    asdasdasfasf

--- src/components/MoonScene/MoonScene.jsx
+++ src/components/MoonScene/MoonScene.jsx
@@ -1,16 +1,15 @@
 import "./MoonScene.css";
-
-const MOON_URL = "http://pngimg.com/uploads/moon/moon_PNG46.png";
-const CLOUD_URL = "https://pngimg.com/uploads/cloud/cloud_PNG112151.png";
+import moonImage from "../../assets/moon.png";
+import cloudImage from "../../assets/cloud.png";
 
 export default function MoonScene() {
   return (
     <div className="moon-scene" aria-hidden="true">
       <div className="scene">
-        <img className="moon" src={MOON_URL} alt="" />
-        <img className="cloud" src={CLOUD_URL} alt="" />
-        <img className="cloud2" src={CLOUD_URL} alt="" />
-        <img className="cloud3" src={CLOUD_URL} alt="" />
+        <img className="moon" src={moonImage} alt="" />
+        <img className="cloud" src={cloudImage} alt="" />
+        <img className="cloud2" src={cloudImage} alt="" />
+        <img className="cloud3" src={cloudImage} alt="" />
         <div className="moon-stars" />
         <div className="moon-twinkle moon-twinkle-1" />
         <div className="moon-twinkle moon-twinkle-2" />
