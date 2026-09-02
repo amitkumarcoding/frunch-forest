18aabdd64aad0b6a9790a3e92d2eb48209f889ef
Amit Kumar <amitaquarius@gmail.com>
2026-09-02 21:10:25 +0530

    asdasdas

--- src/components/MoonScene/MoonScene.jsx
+++ src/components/MoonScene/MoonScene.jsx
@@ -2,14 +2,31 @@ import "./MoonScene.css";
 import moonImage from "../../assets/moon.png";
 import cloudImage from "../../assets/cloud.png";
 
+const randomDelay = (duration) => `-${(Math.random() * duration).toFixed(2)}s`;
+
 export default function MoonScene() {
   return (
     <div className="moon-scene" aria-hidden="true">
       <div className="scene">
         <img className="moon" src={moonImage} alt="" />
-        <img className="cloud" src={cloudImage} alt="" />
-        <img className="cloud2" src={cloudImage} alt="" />
-        <img className="cloud3" src={cloudImage} alt="" />
+        <img
+          className="cloud"
+          src={cloudImage}
+          alt=""
+          style={{ animationDelay: randomDelay(40) }}
+        />
+        <img
+          className="cloud2"
+          src={cloudImage}
+          alt=""
+          style={{ animationDelay: randomDelay(55) }}
+        />
+        <img
+          className="cloud3"
+          src={cloudImage}
+          alt=""
+          style={{ animationDelay: randomDelay(70) }}
+        />
         <div className="moon-stars" />
         <div className="moon-twinkle moon-twinkle-1" />
         <div className="moon-twinkle moon-twinkle-2" />
